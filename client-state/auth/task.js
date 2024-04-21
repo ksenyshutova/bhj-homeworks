890a6f5f6c8d0558d3787401809055a46a94e3b1
ksenyshutova <shutovasn@mail.ru>
2024-04-21 22:01:28 +0300

    update clien-state

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -4,21 +4,27 @@ const welcome = document.getElementById('welcome');
 const form = document.getElementById('signin__form');
 const login = document.getElementsByName('login');
 
+if (localStorage.getItem('id')) {
+    document.getElementById('user_id').textContent = localStorage.getItem('id');
+    signin.classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
+}
+
 form.addEventListener('submit', event => {
     event.preventDefault();
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === xhr.DONE) {
-            const storedName = localStorage.getItem('name');
-            if (storedName) {
-                welcome.classList.add('welcome_active');
-            } else {
-                alert('Неверный логин/пароль');
-                const name = login.textContent;
-                localStorage.setItem('name', name);
-                
-            }
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', () => {
+        let userId = xhr.response.user_id;
+        if (xhr.response.success === true) {
+            document.getElementById('user_id').textContent = userId;
+            signin.classList.remove('signin_active');
+            welcome.classList.add('welcome_active');
+            localStorage.setItem('id', userId);
+        } else {
+            alert('Неверный логин/пароль');
         };
+        form.reset();
     });
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
     const formData = new FormData(form);
--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,12 +1,5 @@
 const textarea = document.getElementById('editor');
-const history = localStorage.getItem('text');
-let text = textarea.value;
-if (history) {
-    textarea.textContent = history;
-} else {
-    textarea.onchange = function () {
-        text = this.value;
-        textarea.insertAdjacentHTML("afterbegin", text);
-        localStorage.setItem('text', text);
-    }
+textarea.value = localStorage.getItem(textarea.name, textarea.value);
+textarea.oninput = function () {
+    localStorage.setItem(textarea.name, textarea.value);
 }
